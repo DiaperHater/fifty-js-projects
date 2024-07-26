00690d62ed8cbe8696503520253725a3c72248fd
Valery Pronin <pownmymail@gmail.com>
2024-07-26 18:57:45 +0300

    feat: close #01 - Expanding Cards

---
+++ 01 - Expanding Cards/assets/js/main.js
@@ -0,0 +1,13 @@
+(function () {
+
+  const cards = document.querySelectorAll(".card")
+
+  cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+      document.querySelector(".card.active").classList.remove("active")
+      card.classList.add('active')
+    })
+  })
+})();

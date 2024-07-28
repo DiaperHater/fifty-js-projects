1339f7fe92173b662e3e2ebcfe9a1a696da88249
Valery Pronin <pownmymail@gmail.com>
2024-07-28 23:46:15 +0300

    refactor: 02 - Progress Steps

--- 02 - Progress Steps/assets/js/main.js
+++ 02 - Progress Steps/assets/js/main.js
@@ -1,60 +1,69 @@
 (function () {
 
+  const root = document.querySelector(":root")
   const stepsCount = document.querySelectorAll(".steps__item").length
   let currentStep = document.querySelectorAll(".steps__item.active").length
-  const prevBtn = document.querySelector(".steps__button--prev")
+
+
+  function nextStep() {
+    const lastActiveItem = Array.from(document.querySelectorAll(".steps__item.active")).pop()
+    const nextActive = lastActiveItem.nextElementSibling
+    nextActive.classList.add("active")
+    root.style.setProperty("--progress-percentage", `calc(100% / ${stepsCount - 1} * ${currentStep})`)
+  }
+
+  function prevStep() {
+    const lastActiveItem = Array.from(document.querySelectorAll(".steps__item.active")).pop()
+    lastActiveItem.classList.remove("active")
+    currentStep = document.querySelectorAll(".steps__item.active").length
+    currentStep == 1 ? root.style.setProperty("--progress-percentage", "0%") : root.style.setProperty("--progress-percentage", `calc( 100% / ${stepsCount - 1} * ${currentStep - 1})`)
+  }
+
+  function updateCurrentStepValue() {
+    currentStep = document.querySelectorAll(".steps__item.active").length
+  }
+
+  function disableButton(btn) {
+    btn.setAttribute("disabled", true)
+  }
+
+  function enableButton(btn) {
+    btn.removeAttribute("disabled")
+  }
+
+
   const nextBtn = document.querySelector(".steps__button--next")
-  const progressBar = document.querySelector(".steps__progress")
-  const root = document.querySelector(":root")
-  const progressPersantage = getComputedStyle(root).getPropertyValue("--progress-percentage")
-
-  console.log(progressPersantage);
-
   nextBtn.addEventListener("click", () => {
 
-    currentStep = document.querySelectorAll(".steps__item.active").length
     if (currentStep < stepsCount) {
-
-      const lastActiveItem = Array.from(document.querySelectorAll(".steps__item.active")).pop()
-      const nextActive = lastActiveItem.nextElementSibling
-      nextActive.classList.add("active")
-
-      root.style.setProperty("--progress-percentage", `calc(${100 / stepsCount * currentStep}% + ${24 * currentStep}px)`)
+      nextStep()
+      updateCurrentStepValue()
     }
 
-    if (currentStep == stepsCount - 1) {
-
-      nextBtn.setAttribute("disabled", true)
+    if (currentStep == stepsCount) {
+      disableButton(nextBtn)
     }
 
-    if (currentStep == 1) {
-
-      prevBtn.removeAttribute("disabled")
+    if (prevBtn.hasAttribute("disabled") && currentStep > 1) {
+      enableButton(prevBtn)
     }
   })
 
+  const prevBtn = document.querySelector(".steps__button--prev")
   prevBtn.addEventListener("click", () => {
 
     currentStep = document.querySelectorAll(".steps__item.active").length
 
     if (currentStep > 1) {
-
-      const lastActiveItem = Array.from(document.querySelectorAll(".steps__item.active")).pop()
-      lastActiveItem.classList.remove("active")
-
-      currentStep = document.querySelectorAll(".steps__item.active").length
-
-      currentStep == 1 ? root.style.setProperty("--progress-percentage", "0%") : root.style.setProperty("--progress-percentage", `calc( 100% / ${stepsCount - 1} * ${currentStep - 1})`)
+      prevStep()
     }
 
     if (currentStep == 1) {
-
-      prevBtn.setAttribute("disabled", true)
+      disableButton(prevBtn)
     }
 
-    if (currentStep < stepsCount && nextBtn.hasAttribute("disabled")) {
-
-      nextBtn.removeAttribute("disabled")
+    if (nextBtn.hasAttribute("disabled") && currentStep < stepsCount) {
+      enableButton(nextBtn)
     }
   })
 

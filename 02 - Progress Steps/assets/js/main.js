0ba9f01e1b45ce846b926ea6560ea94829e1c87e
Valery Pronin <pownmymail@gmail.com>
2024-07-29 00:25:34 +0300

    refactor: 02 - Progress Steps (course author version)

--- 02 - Progress Steps/assets/js/main.js
+++ 02 - Progress Steps/assets/js/main.js
@@ -1,70 +1,47 @@
 (function () {
 
   const root = document.querySelector(":root")
-  const stepsCount = document.querySelectorAll(".steps__item").length
-  let currentStep = document.querySelectorAll(".steps__item.active").length
+  const next = document.querySelector(".steps__button--next")
+  const prev = document.querySelector(".steps__button--prev")
+  const steps = document.querySelectorAll(".steps__item")
+  let currentActive = 1
 
-
-  function nextStep() {
-    const lastActiveItem = Array.from(document.querySelectorAll(".steps__item.active")).pop()
-    const nextActive = lastActiveItem.nextElementSibling
-    nextActive.classList.add("active")
-    root.style.setProperty("--progress-percentage", `calc(100% / ${stepsCount - 1} * ${currentStep})`)
-  }
-
-  function prevStep() {
-    const lastActiveItem = Array.from(document.querySelectorAll(".steps__item.active")).pop()
-    lastActiveItem.classList.remove("active")
-    currentStep = document.querySelectorAll(".steps__item.active").length
-    currentStep == 1 ? root.style.setProperty("--progress-percentage", "0%") : root.style.setProperty("--progress-percentage", `calc( 100% / ${stepsCount - 1} * ${currentStep - 1})`)
-  }
-
-  function updateCurrentStepValue() {
-    currentStep = document.querySelectorAll(".steps__item.active").length
-  }
-
-  function disableButton(btn) {
-    btn.setAttribute("disabled", true)
-  }
-
-  function enableButton(btn) {
-    btn.removeAttribute("disabled")
-  }
-
-
-  const nextBtn = document.querySelector(".steps__button--next")
-  nextBtn.addEventListener("click", () => {
-
-    if (currentStep < stepsCount) {
-      nextStep()
-      updateCurrentStepValue()
+  next.addEventListener("click", () => {
+    currentActive++
+    if (currentActive > steps.length) {
+      currentActive = steps.length
     }
 
-    if (currentStep == stepsCount) {
-      disableButton(nextBtn)
-    }
-
-    if (prevBtn.hasAttribute("disabled") && currentStep > 1) {
-      enableButton(prevBtn)
-    }
+    update()
   })
 
-  const prevBtn = document.querySelector(".steps__button--prev")
-  prevBtn.addEventListener("click", () => {
-
-    currentStep = document.querySelectorAll(".steps__item.active").length
-
-    if (currentStep > 1) {
-      prevStep()
+  prev.addEventListener("click", () => {
+    currentActive--
+    if (currentActive < 1) {
+      currentActive = 1
     }
 
-    if (currentStep == 1) {
-      disableButton(prevBtn)
-    }
-
-    if (nextBtn.hasAttribute("disabled") && currentStep < stepsCount) {
-      enableButton(nextBtn)
-    }
+    update()
   })
 
+  function update() {
+    steps.forEach((step, index) => {
+      if (index < currentActive) {
+        step.classList.add("active")
+      } else {
+        step.classList.remove("active")
+      }
+    })
+
+    if (currentActive === steps.length) {
+      next.disabled = true
+    } else if (currentActive === 1) {
+      prev.disabled = true
+    } else {
+      next.disabled = false
+      prev.disabled = false
+    }
+
+    root.style.setProperty("--progress-percentage", `calc(100% / ${steps.length - 1} * ${currentActive - 1})`)
+  }
 })();

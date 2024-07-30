e075daa274f070483dbd449c5e53657fe1ae0264
Valery Pronin <pownmymail@gmail.com>
2024-07-30 11:50:23 +0300

    04 - feat: course author version

--- 04 - Hidden Search Widget/assets/js/main.js
+++ 04 - Hidden Search Widget/assets/js/main.js
@@ -1,4 +1,11 @@
 (function () {
 
-  console.log("My Project Starter");
+  const btn = document.querySelector(".search-widget__btn")
+  const search = document.querySelector(".search-widget")
+  const input = document.querySelector(".search-widget__input")
+
+  btn.addEventListener("click", () => {
+    search.classList.toggle("active")
+    input.focus()
+  })
 })();

1fc2b8476b3fa34da3ecdc693759d9b4140086bf
Valery Pronin <pownmymail@gmail.com>
2024-08-05 01:47:37 +0300

    11: refactor to functional approach

--- 11 - Key Code/assets/js/main.js
+++ 11 - Key Code/assets/js/main.js
@@ -1,4 +1,38 @@
-import App from './App.js'
+(function () {
+  "use strict";
+  if (typeof document === "undefined") {
+    // Not on browser
+    return;
+  }
 
-const app = new App()
-app.run()
+  document.addEventListener("DOMContentLoaded", init);
+
+  function init() {
+
+    window.addEventListener("keypress", event => updateView(event.key, event.code))
+    createNewBlock("event.key", "")
+    createNewBlock("event.code", "")
+  }
+
+  function updateView(key, code) {
+    const blocksContainer = document.querySelector(".keycode__container")
+    blocksContainer.innerHTML = ""
+
+    createNewBlock("event.key", key)
+    createNewBlock("event.code", code)
+  }
+
+  function createNewBlock(name, value) {
+    const blocksContainer = document.querySelector(".keycode__container")
+    const block = document.createElement("div")
+    blocksContainer.appendChild(block)
+    block.outerHTML = `
+      <div class="keycode__block block-keycode">
+        <div class="block-keycode__name">${name}</div>
+        <div class="block-keycode__inner">
+          <span class="block-keycode__value">${value}</span>
+        </div>
+      </div>
+    `
+  }
+})()
